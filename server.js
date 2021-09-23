100ccdcf4a65c24d633957157e91ba27cbb18438
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-23 12:02:26 -0700

    2

--- server.js
+++ server.js
@@ -3,4 +3,15 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
-server.listen(3000)
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+
+})
+
+app.get('/:room', (req,res)=>{
+    
+})
+
+server.listen(3000) 
