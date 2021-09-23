9e8e33ec508c163df066c257783e1ec8554abcc3
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-23 13:55:49 -0700

    1

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 const socket = io('/')
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: 3001
+});
 
 socket.emit('join-room', ROOM_ID, 10)
+ 
 
 socket.on('User-connected', userId => {
  console.log('User connected: ' + userId)
